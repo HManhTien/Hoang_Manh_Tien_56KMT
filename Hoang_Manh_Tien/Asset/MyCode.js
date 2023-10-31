cbb4f1969f21c06646e1f7e845b5c4342e84b52d
HoangTien <hoangtienvhdh@gmail.com>
2023-11-01 01:26:02 +0700

    đi ngủ

--- Hoang_Manh_Tien/Asset/MyCode.js
+++ Hoang_Manh_Tien/Asset/MyCode.js
@@ -1,226 +1,70 @@
 ﻿$(document).ready(function () {
-    //code js here mẫu của jquery
-    const api = '/api.aspx';
-    function delete_company(id, json) {
-        var cty;
-        for (var item of json.data) {
-            if (item.id == id) {
-                cty = item;
-                break;
-            }
+
+    $('#nut-dang-ky').click(function () {
+        var content =
+        `
+        <style>
+        .form-group {
+            display: flex;
+            align-items: center;
+            margin-bottom: 10px;
         }
-        //xác nhận trước khi xóa
-        var dialog_xoa = $.confirm({
-            title: `Xác nhận xóa công ty ${cty.name}`,
-            content: `Xác nhận xóa nhé????`,
-            buttons: {
-                YES: {
-                    action: function () {
-                        var data_gui_di = {
-                            action: 'delete_company',
-                            id: id, //gửi đi id của cty cần xóa: api, sp sẽ làm phần còn lại
-                        }
-                        $.post(api, data_gui_di, function (data) {
-                            //đợi data là json string text gửi về
-                            var json = JSON.parse(data); //json string text => obj
-                            if (json.ok) { //dùng obj
-                                dialog_xoa.close();
-                                cap_nhap_company();  //vẽ lại kq mới
-                            } else {
-                                alert(json.msg) // lỗi gì ở trên lo, ta cứ show ra thôi
-                            }
-                        })
-                    }
-                },
-                NO: {
-
-                }
-            }
-        })
-    }
-    function edit_company(id, json) {
-        //show 1 dialog: điền sẵn các thông của công ty cần edit
-        //chờ bấm save -> gọi api: truyền đi các value đã bị edit -> nhận về json kết quả
-
-        //json và id => cty
-        var cty;
-        for (var item of json.data) {
-            if (item.id == id) {
-                cty = item;
-                break;
-            }
+        label {
+            width: 200px;
+            text-align: right;
+            padding-right: 10px;
         }
-        var content = `form điền sẵn
-    tên: <input type=text id="edit-name" value="${cty.name}"> <br>
-    d/c: <input type=text id="edit-address" value="${cty.address}"> 
-    `
-        var dialog_edit = $.confirm({
-            title: 'edit công ty',
+        input[type="text"] {
+            flex: 1;
+            border: 1px solid #ccc;
+            border-radius: 10px;
+            padding: 5px;                  
+        }      
+        </style>
+             <div class="form-group">
+                <label for="create-name">Name :</label>
+                <input type="text" id="create-name" placeholder="Your name" required>
+            </div>
+            <div class="form-group">
+                <label for="create-pw">Mật khẩu :</label>
+                <input type="text" id="create-pw" placeholder="Your password" required>
+            </div>
+            <div class="form-group">
+                <label for="create-pw2">Xác Nhận Mật Khẩu :</label>
+                <input type="text" id="create-pw2" placeholder="Confirm your password" required>
+            </div>
+        
+        `
+        $.confirm({
+            title: 'Đăng Ký!',
             content: content,
-            columnClass: 'large',
+
             buttons: {
-                save: {
+                formSubmit: {
+                    text: 'Đăng ký',
+                    btnClass: 'btn-primary',
                     action: function () {
-                        var data_gui_di = {
-                            action: 'edit_company',
-                            name: $('#edit-name').val(),
-                            address: $('#edit-address').val(),
-                            id: id,
-                            lat: 0,
-                            lng: 1,
-                            phone: '113',
-                            zalo: 'zzz'
+                        var name = this.$content.find('.name').val();
+                        if (!name) {
+                            $.alert('provide a valid name');
+                            return false;
                         }
-                        //console.log(data_gui_di);
-                        $.post(api, data_gui_di, function (data) {
-                            var json = JSON.parse(data);
-                            if (json.ok) {
-                                dialog_edit.close();
-                                cap_nhap_company();
-                            } else {
-                                alert(json.msg)
-                            }
-                        })
+                        $.alert('Your name is ' + name);
                     }
                 },
-                cancel: {
-
-                }
-            }
-        })
-    }
-
-    function cap_nhap_company() {
-        $.post(api,
-            {
-                action: 'list_company'
-            },
-            function (data) {
-                //alert(data)
-                var json = JSON.parse(data); //txt trong data -> obj json
-                var noidung_ds_cty_html = "";
-                if (json.ok) {
-                    noidung_ds_cty_html += `<table class="table table-hover">
-              <thead>
-              <tr>
-                <th>STT</th>
-                <th>Tên</th>
-                <th>Đ/C</th>
-                <th>Phone</th>
-                <th>Sửa/xóa</th>
-              </tr>
-              </thead><tbody>`;
-                    //duyet json -> noidung_ds_cty_html xịn
-                    var stt = 0;
-                    for (var cty of json.data) {
-                        //sua_xoa là 2 nút: mỗi nút kèm theo data để sau này phân loại: là data-cid  và data-action
-                        var sua_xoa = `<button class="btn btn-sm btn-warning nut-sua-xoa" data-cid="${cty.id}" data-action="edit_company">Sửa</button>`;
-                        sua_xoa += ` <button class="btn btn-sm btn-danger nut-sua-xoa" data-cid="${cty.id}" data-action="delete_company">Xóa</button>`;
-                        noidung_ds_cty_html += `
-                <tr>
-                <td>${++stt}</td>
-                <td>${cty.name}</td>
-                <td>${cty.address}</td>
-                <td>${cty.phone}</td>
-                <td>${sua_xoa}</td>
-              </tr>`;
-                    }
-
-                    noidung_ds_cty_html += "</tbody></table>";
-                } else {
-
-                    noidung_ds_cty_html = "không có dữ liệu";
-                }
-                //đưa html vừa nối nối vào chỗ định trước: #ds_cong_ty
-                $('#ds_cong_ty').html(noidung_ds_cty_html); //gán html vào thân dialog
-
-                //trong html vừa đua vào có nhiều nút sửa và xóa, đều có class nut-sua-xoa
-                //selector này tóm đc mọi nút
-                $('.nut-sua-xoa').click(function () {
-                    //phân biệt các nút bằng data kèm theo
-                    var action = $(this).data('action')  //lấy action kèm theo
-                    var id = $(this).data('cid')  //lấy cid kèm theo
-                    if (action == 'delete_company') { //dùng action
-                        //can xac nhan
-                        delete_company(id, json); //dùng id vào đây để hàm này xử, cho khỏi rối code
-                    } else if (action == 'edit_company') {
-                        //ko can xac nhan
-                        edit_company(id, json);
-                    }
-                });
-            })
-    }
-
-    function add_company() {
-        //show 1 dialog, các truongf để trông để user nhập
-        //sau đó thu nhận các value đã input, có thể check đk trước khi gửi
-        //gửi api, thu về json, ok=1 =>cap_nhap_company
-        var content = `html form cho input các trường của 1 cty mới:..<hr>
-    Name: <input id="nhap-name"><br>
-    Address: <input id="nhap-address">
-    `
-        var dialog_add = $.confirm({
-            title: 'THêm mới 1 công ty',
-            content: content,
-            buttons: {
-                save: {
-                    text: 'Thêm cty vào db',
-                    action: function () {
-                        //sau đó thu nhận các value đã input, có thể check đk trước khi gửi
-                        //gửi api, thu về json, ok=1 =>cap_nhap_company
-                        var data_gui_di = {
-                            action: 'add_company',
-                            name: $('#nhap-name').val(),
-                            address: $('#nhap-address').val(),
-                            lat: 0,
-                            lng: 1,
-                            phone: '555',
-                            zalo: 'ttttttttzzz'
-                        }
-                        //console.log(data_gui_di);
-                        $.post(api, data_gui_di, function (data) {
-                            var json = JSON.parse(data);
-                            if (json.ok) {
-                                dialog_add.close();
-                                cap_nhap_company();
-                            } else {
-                                alert(json.msg)
-                            }
-                        });
-                    }//het save
+                cancel: function () {
+                    //close
                 },
-                xxx: {}
-            }//button
-        });
-    }
-    function list_company() {
-        var dialog_list_company = $.confirm({
-            title: "Ds công ty",
-            content: `<div id="ds_cong_ty">loading...</div>`,
-            columnClass: 'large',
-            buttons: {
-                add: {
-                    text: 'Thêm công ty',
-                    action: function () {
-                        add_company();
-                        return false; // ko đóng dialog_list_company
-                    }
-                },
-                close: {
-
-                }
             },
             onContentReady: function () {
-                //alert('dialog show ok')
-                //hoi api: ds cong ty la json nao?
-                cap_nhap_company(); //fill html vào thêm dialog tại div#ds_cong_ty
+                // bind to events
+                var jc = this;
+                this.$content.find('form').on('submit', function (e) {
+                    // if the user submits the form by pressing enter in the field.
+                    e.preventDefault();
+                    jc.$$formSubmit.trigger('click'); // reference the button and click it
+                });
             }
-        });
-    }
-    $('#nut-dang-nhap').click(function () {
-        $.alert({
-            title: 'Alert!',
-            content: 'Simple alert!',
         });
     });
 });
--- Hoang_Manh_Tien/api.aspx.cs
+++ Hoang_Manh_Tien/api.aspx.cs
@@ -4,14 +4,56 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using SuatAn;
 
 namespace Hoang_Manh_Tien
 {
     public partial class api : System.Web.UI.Page
     {
+        void xuly_company(string action)
+        {
+            //khai báo biến db thuộc lớp SqlServer
+            SqlServer db = new SqlServer();
+            SqlCommand cm = db.GetCmd("SP_Company", action); //tạo cm với "SP_Company" và @action từ method GetCmd của db
+            switch (action)
+            {
+                //2 loại này truyền 5 tham số chung
+                case "add_company":
+                case "edit_company":
+                    cm.Parameters.Add("@name", SqlDbType.NVarChar, 255).Value = Request["name"];
+                    cm.Parameters.Add("@address", SqlDbType.NVarChar, 255).Value = Request["address"];
+                    cm.Parameters.Add("@lat", SqlDbType.Float).Value = Request["lat"];
+                    cm.Parameters.Add("@lng", SqlDbType.Float).Value = Request["lng"];
+                    cm.Parameters.Add("@phone", SqlDbType.VarChar, 10).Value = Request["phone"];
+                    break;
+
+            }
+            switch (action)
+            {
+                //2 loại này cần thêm @id
+                case "edit_company":
+                case "delete_company":
+                    cm.Parameters.Add("@id", SqlDbType.Int).Value = Request["id"];
+                    break;
+            }
+            string json = (string)db.Scalar(cm); //thuc thi SqlCommand cm này để thu về json
+            Response.Write(json); //trả json về trình duyệt
+        }
+
         protected void Page_Load(object sender, EventArgs e)
         {
-            
+            string action = Request["action"];
+            switch (action)
+            {
+                case "list_company":
+                case "add_company":
+                case "edit_company":
+                case "delete_company":
+                    xuly_company(action);
+                    break;
+            }
         }
     }
 }
