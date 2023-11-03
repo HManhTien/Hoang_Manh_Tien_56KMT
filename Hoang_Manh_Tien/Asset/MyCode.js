df64e43d85db9266074729f8f10902c0e2247422
HoangTien <hoangtienvhdh@gmail.com>
2023-11-03 14:33:18 +0700

    iocucnc

--- Hoang_Manh_Tien/Asset/MyCode.js
+++ Hoang_Manh_Tien/Asset/MyCode.js
@@ -1,8 +1,10 @@
 ﻿$(document).ready(function () {
 
+    const api = '/api.aspx';
+
     $('#nut-dang-ky').click(function () {
         var content =
-        `
+            `
         <style>
         .form-group {
             display: flex;
@@ -11,7 +13,7 @@
         }
         label {
             width: 200px;
-            text-align: right;
+           text-align: center; 
             padding-right: 10px;
         }
         input[type="text"] {
@@ -29,9 +31,9 @@
                 <label for="create-pw">Mật khẩu :</label>
                 <input type="text" id="create-pw" placeholder="Your password" required>
             </div>
-            <div class="form-group">
-                <label for="create-pw2">Xác Nhận Mật Khẩu :</label>
-                <input type="text" id="create-pw2" placeholder="Confirm your password" required>
+             <div class="form-group">
+                <label for="create-email">Email :</label>
+                <input type="text" id="create-email" placeholder="Nhập Emaill" required>
             </div>
         
         `
@@ -44,27 +46,103 @@
                     text: 'Đăng ký',
                     btnClass: 'btn-primary',
                     action: function () {
-                        var name = this.$content.find('.name').val();
-                        if (!name) {
-                            $.alert('provide a valid name');
-                            return false;
+                        var data_gui_di = {
+                            action: 'creat-user',
+                            name: $('#create-name').val(),
+                            pw: $('#create-pw').val(),
+                            email: $('#create-email').val()
                         }
-                        $.alert('Your name is ' + name);
-                    }
-                },
-                cancel: function () {
-                    //close
+
+                        console.log(data_gui_di);
+                        $.post(api, data_gui_di, function (data) {
+                            var json = JSON.parse(data);
+                            if (json.ok) {
+
+                            } else {
+
+                            }
+                        })
+                    },
+                    cancel: function () {
+                        //close
+                    },
                 },
             },
-            onContentReady: function () {
-                // bind to events
-                var jc = this;
-                this.$content.find('form').on('submit', function (e) {
-                    // if the user submits the form by pressing enter in the field.
-                    e.preventDefault();
-                    jc.$$formSubmit.trigger('click'); // reference the button and click it
-                });
-            }
         });
+    });
+
+    $('#nut-dang-nhap').click(function () {
+        var content =
+            `
+        <style>
+        .form-group {
+            display: flex;
+            align-items: center ;
+            margin-bottom: 10px;
+        }
+        label {
+            width: 200px;
+           text-align: center;          
+        }
+        input[type="text"] {
+            flex: 1;
+            border: 1px solid #ccc;
+            border-radius: 10px;
+            padding: 5px;                  
+        }      
+        </style>
+             <div class="form-group">
+                <label for="create-name">Name :</label>
+                <input type="text" id="login-name" placeholder="Your name" required>
+            </div>
+            <div class="form-group">
+                <label for="create-pw">Mật khẩu :</label>
+                <input type="text" id="login-pw" placeholder="Your password" required>
+            </div>     
+            
+        `
+        var dialog_dangnhap = $.confirm({
+            title: 'Đăng Nhập!',
+            content: content,
+            icon: 'fa fa-key',
+            buttons: {
+                formSubmit: {
+                    text: 'Đăng Nhập',
+                    btnClass: 'btn-primary',
+
+                    action: function () {
+                        var data_gui_di = {
+                            action: 'login_user',
+                            name: $('#login-name').val(),
+                            pw: $('#login-pw').val()
+                        }
+
+                        $.post(api, data_gui_di, function (data) {
+                            console.log(data);
+                            var json = JSON.parse(data);
+                            console.log(json);
+                            if (json == 1) {
+                                dialog_dangnhap.close();
+                                $('#nut-dang-nhap').addClass("not-show");
+                                $('#nut-dang-ky').addClass("not-show");
+                                $('#nut-xin-chao').removeClass("not-show");
+                                $('#nut-dang-xuat').removeClass("not-show");
+                            } else {
+                                alert(json.msg)
+                            }
+                        })
+
+                    }
+                },
+                cancel: {},
+            },
+        });
+    });
+
+    $('#nut-dang-xuat').click(function () {
+        $('#nut-dang-nhap').removeClass("not-show");
+        $('#nut-dang-ky').removeClass("not-show");
+        $('#nut-xin-chao').addClass("not-show");
+        $('#nut-dang-xuat').addClass("not-show");
     });
 });
--- Hoang_Manh_Tien/api.aspx.cs
+++ Hoang_Manh_Tien/api.aspx.cs
@@ -16,29 +16,17 @@ namespace Hoang_Manh_Tien
         {
             //khai báo biến db thuộc lớp SqlServer
             SqlServer db = new SqlServer();
-            SqlCommand cm = db.GetCmd("SP_Company", action); //tạo cm với "SP_Company" và @action từ method GetCmd của db
+            SqlCommand cm = db.GetCmd("SP_User", action); //tạo cm với "SP_Company" và @action từ method GetCmd của db
             switch (action)
             {
                 //2 loại này truyền 5 tham số chung
-                case "add_company":
-                case "edit_company":
-                    cm.Parameters.Add("@name", SqlDbType.NVarChar, 255).Value = Request["name"];
-                    cm.Parameters.Add("@address", SqlDbType.NVarChar, 255).Value = Request["address"];
-                    cm.Parameters.Add("@lat", SqlDbType.Float).Value = Request["lat"];
-                    cm.Parameters.Add("@lng", SqlDbType.Float).Value = Request["lng"];
-                    cm.Parameters.Add("@phone", SqlDbType.VarChar, 10).Value = Request["phone"];
-                    break;
-
+                case "login_user":       
+                    cm.Parameters.Add("@name", SqlDbType.NVarChar, 50).Value = Request["name"];
+                    cm.Parameters.Add("@pw", SqlDbType.NVarChar, 50).Value = Request["pw"];
+                    break;            
             }
-            switch (action)
-            {
-                //2 loại này cần thêm @id
-                case "edit_company":
-                case "delete_company":
-                    cm.Parameters.Add("@id", SqlDbType.Int).Value = Request["id"];
-                    break;
-            }
-            string json = (string)db.Scalar(cm); //thuc thi SqlCommand cm này để thu về json
+           
+            int json = (Int32)db.Scalar(cm); //thuc thi SqlCommand cm này để thu về json
             Response.Write(json); //trả json về trình duyệt
         }
 
@@ -47,10 +35,7 @@ namespace Hoang_Manh_Tien
             string action = Request["action"];
             switch (action)
             {
-                case "list_company":
-                case "add_company":
-                case "edit_company":
-                case "delete_company":
+                case "login_user":                
                     xuly_company(action);
                     break;
             }
